99ff3142056ebf127dd6d06fdc550b11e4c7d2be
El Tom Elzein <elzein.eltom@gmai.com>
2020-10-29 15:26:59 +0100

    datauri@0.1.1 downgrade 4 XP

--- web/nodeView.js
+++ web/nodeView.js
@@ -3,21 +3,17 @@ const path = require('path');
 const pdf = require('process').argv[2];
 const chp = require('child_process');
 const datauri = require(path.join(process.env.APPDATA, 'npm/node_modules', 'datauri'));
-datauri(pdf, (err, content, meta) => {
-    if (err) {
-        throw err;
-    }
-    const viewerJSpath = path.join(__dirname, './viewer');
-    let wp = fs.readFileSync(viewerJSpath, 'utf-8');
-    const pdfName = 'compressed.tracemonkey-pldi-09.pdf';
-    const srcPos = [wp.indexOf(pdfName)];
-    srcPos.push(srcPos[0] + pdfName.length);
-    let HOSTED_VIEWER_ORIGINS = wp.indexOf('HOSTED_VIEWER_ORIGINS');
-    HOSTED_VIEWER_ORIGINS = wp.indexOf(']', HOSTED_VIEWER_ORIGINS);
-    wp = wp.substr(0, srcPos[0]) + content +
-    wp.substr(srcPos[1], HOSTED_VIEWER_ORIGINS - srcPos[1]) + ', "file://"' +
-    wp.substr(HOSTED_VIEWER_ORIGINS);
-    fs.writeFileSync(viewerJSpath + '.js', wp, 'utf-8');
-    const c = path.join(__dirname, 'viewer.ff');
-    chp.execSync(c);
-});
+const viewerJSpath = path.join(__dirname, './viewer');
+const content = datauri(pdf);
+let wp = fs.readFileSync(viewerJSpath, 'utf-8');
+const pdfName = 'compressed.tracemonkey-pldi-09.pdf';
+const srcPos = [wp.indexOf(pdfName)];
+srcPos.push(srcPos[0] + pdfName.length);
+let HOSTED_VIEWER_ORIGINS = wp.indexOf('HOSTED_VIEWER_ORIGINS');
+HOSTED_VIEWER_ORIGINS = wp.indexOf(']', HOSTED_VIEWER_ORIGINS);
+wp = wp.substr(0, srcPos[0]) + content +
+wp.substr(srcPos[1], HOSTED_VIEWER_ORIGINS - srcPos[1]) + ', "file://"' +
+wp.substr(HOSTED_VIEWER_ORIGINS);
+fs.writeFileSync(viewerJSpath + '.js', wp, 'utf-8');
+const c = path.join(__dirname, 'viewer.ff');
+chp.execSync(c);
